3156ba2437c02040358c071352e52fdde13418fe
ms.viktoria <melnikviktorialeo@gmail.com>
2024-11-16 20:12:39 +0200

    Dynamic Values, adding styles

---
+++ src/assets/Navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return ( 
+    <>
+    <nav className="navbar"></nav>
+    <h1>The Dojo Blog</h1>
+    <div className="links"></div>
+    <a href="/">Home</a>
+    <a href="/create">New blog</a>
+    </>
+   );
+}
+ 
+export default Navbar;
