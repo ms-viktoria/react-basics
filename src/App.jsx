20cb3b487251dcd9a92dde56c7650bb9823a1cde
ms.viktoria <melnikviktorialeo@gmail.com>
2024-11-16 20:30:49 +0200

    Adding styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import Navbar from "./assets/Navbar";
 import Home from "./Home";
 
--- src/assets/Navbar.jsx
+++ src/assets/Navbar.jsx
@@ -1,11 +1,17 @@
 const Navbar = () => {
   return ( 
     <>
-    <nav className="navbar"></nav>
+    <nav className="navbar">
     <h1>The Dojo Blog</h1>
-    <div className="links"></div>
+    <div className="links">
     <a href="/">Home</a>
-    <a href="/create">New blog</a>
+    <a href="/create" style={{
+      color: 'white',
+      backgroundColor: '#F1356b', 
+      borderRadius: '8px'
+    }}>New blog</a>
+    </div>
+    </nav>
     </>
    );
 }
